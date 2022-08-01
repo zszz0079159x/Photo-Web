e5078575468f45404081b41c44ddb3924baf8021
C4tt <zszz0079159x@gmail.com>
2022-08-01 23:54:12 +0800

    update input width

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "../components/Search";
-import Nav from "../components/Nav";
-import Footer from "../components/Footer";
-import { useState } from "react";
 import Pictures from "../components/Pictures";
 
 const Homepage = () => {
   const [input, setInput] = useState("");
   let [data, setData] = useState(null);
   let [page, setPage] = useState(1);
-  let [current, setCurrent] = useState("");
+  const [current, setCurrent] = useState("");
   const auth = "563492ad6f9170000100000146ccb74e82ed41bebb31a07c9a4d53fb";
   const initialUrl = "https://api.pexels.com/v1/curated?page=1&per_page=15";
   const searchUrl = `https://api.pexels.com/v1/search?query=${current}&per_page=15&page=1`;
@@ -23,17 +20,16 @@ const Homepage = () => {
         Authorization: auth,
       },
     });
-
     let parseData = await dataFetch.json();
     setData(parseData.photos);
   };
 
   const morepic = async () => {
     let newUrl;
-    if (input === "") {
+    if (current === "") {
       newUrl = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
     } else {
-      newUrl = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=${page}`;
+      newUrl = `https://api.pexels.com/v1/search?query=${current}&per_page=15&page=${page}`;
     }
     setPage(page + 1);
     const dataFetch = await fetch(newUrl, {
@@ -43,7 +39,6 @@ const Homepage = () => {
         Authorization: auth,
       },
     });
-
     let parseData = await dataFetch.json();
     setData(data.concat(parseData.photos));
   };
@@ -74,6 +69,7 @@ const Homepage = () => {
             return <Pictures data={d} />;
           })}
       </div>
+
       <div className="more">
         <button onClick={morepic}>Load More</button>
       </div>
